d65758e2ba089f475b1f68f3f0da6e40bfa3226e
Mihail Chilyashev <chilyashev@gmail.com>
2024-02-19 20:08:44 +0200

    Optimize database queries

--- setup.py
+++ setup.py
@@ -66,3 +66,4 @@
 2024-07-19 04:26:17.555683: Update documentation and README
 2024-02-09 02:05:41.563477: Improve error handling
 2024-04-04 17:23:49.641350: Add unit tests for core modules
+2024-02-19 20:08:44.648658: Optimize database queries
