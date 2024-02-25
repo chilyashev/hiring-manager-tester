7d30b30e2a8da532698d8ed1e3b70418459220c8
Mihail Chilyashev <chilyashev@gmail.com>
2024-02-25 15:39:43 +0200

    Remove deprecated functions

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
 2024-07-05 13:16:26.975676: Update documentation and README
 2024-05-27 07:47:45.055812: Add new feature to user interface
 2024-02-15 20:17:09.109504: Optimize database queries
+2024-02-25 15:39:43.197561: Remove deprecated functions
