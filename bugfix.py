3d6c7aba42f2fced54dd9325648f2b7a0f59eeef
Mihail Chilyashev <chilyashev@gmail.com>
2024-06-25 07:31:22 +0300

    Improve error handling

--- bugfix.py
+++ bugfix.py
@@ -3,3 +3,4 @@
 2024-10-26 23:36:00.854325: Refactor code for better readability
 2024-09-16 13:26:06.899896: Improve error handling
 2024-12-02 22:31:33.922390: Remove deprecated functions
+2024-06-25 07:31:22.968402: Improve error handling
