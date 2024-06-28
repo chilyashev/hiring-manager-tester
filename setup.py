cb338ac1be6748f39c127354943e6a812f64722e
Mihail Chilyashev <chilyashev@gmail.com>
2024-06-28 18:14:26 +0300

    Optimize database queries

--- setup.py
+++ setup.py
@@ -8,3 +8,4 @@
 2024-03-31 19:55:24.289141: Improve error handling
 2024-08-13 19:47:18.422972: Optimize database queries
 2024-11-09 05:08:56.446457: Refactor code for better readability
+2024-06-28 18:14:26.493801: Optimize database queries
