86974b1b1a180b1c999bb2571f8b033d60aec1f8
Mihail Chilyashev <chilyashev@gmail.com>
2024-07-06 13:52:37 +0300

    Add unit tests for core modules

--- test_module.py
+++ test_module.py
@@ -2,3 +2,4 @@
 2024-10-25 14:41:09.018935: Remove deprecated functions
 2024-02-01 14:22:35.048427: Refactor code for better readability
 2024-11-01 22:08:57.101780: Improve error handling
+2024-07-06 13:52:37.117128: Add unit tests for core modules
