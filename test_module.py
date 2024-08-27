8cb3553be992a7b09fd977a586164018558a764e
Mihail Chilyashev <chilyashev@gmail.com>
2024-08-27 23:00:56 +0300

    Remove deprecated functions

--- test_module.py
+++ test_module.py
@@ -3,3 +3,4 @@
 2024-02-01 14:22:35.048427: Refactor code for better readability
 2024-11-01 22:08:57.101780: Improve error handling
 2024-07-06 13:52:37.117128: Add unit tests for core modules
+2024-08-27 23:00:56.125858: Remove deprecated functions
