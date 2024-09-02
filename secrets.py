cb8cfe80aec848dc198ac91940a5e52ba7a12563
Mihail Chilyashev <chilyashev@gmail.com>
2024-09-02 23:08:33 +0300

    Remove deprecated functions

--- secrets.py
+++ secrets.py
@@ -31,3 +31,4 @@
 2024-12-26 12:59:17.019347: Add new feature to user interface
 2024-08-04 23:54:45.073868: Fix bug in data processing pipeline
 2024-01-24 06:22:00.179788: Add unit tests for core modules
+2024-09-02 23:08:33.203228: Remove deprecated functions
