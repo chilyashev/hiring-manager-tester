9cb0926ac6b79c5648b0d26a7a13ac1d162df0d5
Mihail Chilyashev <chilyashev@gmail.com>
2024-09-16 13:26:06 +0300

    Improve error handling

--- bugfix.py
+++ bugfix.py
@@ -1,3 +1,4 @@
 2024-04-27 19:25:09.810485: Update documentation and README
 2024-08-23 09:30:08.839430: Update dependencies
 2024-10-26 23:36:00.854325: Refactor code for better readability
+2024-09-16 13:26:06.899896: Improve error handling
