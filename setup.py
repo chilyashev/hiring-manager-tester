610fe9c77eaf096f478948a95d6e3c292948f565
Mihail Chilyashev <chilyashev@gmail.com>
2024-09-17 14:31:16 +0300

    Remove deprecated functions

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 2024-11-18 07:03:38.847160: Fix bug in data processing pipeline
 2024-09-13 04:46:41.930140: Refactor code for better readability
 2024-10-13 16:19:51.938177: Add new feature to user interface
+2024-09-17 14:31:16.953892: Remove deprecated functions
