fc43656493aae1d30199e4edb31cdb6ebfcfdd25
Mihail Chilyashev <chilyashev@gmail.com>
2024-10-31 07:44:01 +0200

    Optimize database queries

--- utils.py
+++ utils.py
@@ -17,3 +17,4 @@
 2024-02-24 17:53:08.837072: Remove deprecated functions
 2024-06-09 19:01:42.025079: Optimize database queries
 2025-01-03 21:37:37.040979: Optimize database queries
+2024-10-31 07:44:01.794716: Optimize database queries
