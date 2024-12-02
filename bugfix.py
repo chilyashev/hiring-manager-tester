ced0981715501870664b40b00d6068ec2c12a5bd
Mihail Chilyashev <chilyashev@gmail.com>
2024-12-02 22:31:33 +0200

    Remove deprecated functions

--- bugfix.py
+++ bugfix.py
@@ -2,3 +2,4 @@
 2024-08-23 09:30:08.839430: Update dependencies
 2024-10-26 23:36:00.854325: Refactor code for better readability
 2024-09-16 13:26:06.899896: Improve error handling
+2024-12-02 22:31:33.922390: Remove deprecated functions
