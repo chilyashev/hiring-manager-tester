96bec950a824717a47b5b04c4c47e4f53b642039
Mihail Chilyashev <chilyashev@gmail.com>
2025-01-21 20:01:29 +0200

    Update documentation and README

--- setup.py
+++ setup.py
@@ -74,3 +74,4 @@
 2025-02-04 02:12:30.305802: Update dependencies
 2025-02-11 13:46:28.404783: Improve performance of data processing
 2025-01-21 21:47:47.440316: Improve error handling
+2025-01-21 20:01:29.498253: Update documentation and README
