eed0f1f8fd689c2babc9e3168135c55ae50eac3a
Mihail Chilyashev <chilyashev@gmail.com>
2025-02-13 02:06:47 +0200

    Remove deprecated functions

--- secrets.py
+++ secrets.py
@@ -48,3 +48,4 @@
 2025-01-16 04:16:50.096119: Remove deprecated functions
 2025-02-03 10:34:23.110645: Improve error handling
 2025-03-06 07:34:11.147799: Remove deprecated functions
+2025-02-13 02:06:47.154987: Remove deprecated functions
