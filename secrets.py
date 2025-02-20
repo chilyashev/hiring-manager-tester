1cf4b2dd392c5a70907668f0797911da8dad2280
Mihail Chilyashev <chilyashev@gmail.com>
2025-02-20 10:19:08 +0200

    Update documentation and README

--- secrets.py
+++ secrets.py
@@ -56,3 +56,4 @@
 2025-02-06 06:30:29.334141: Add new feature to user interface
 2025-01-25 09:09:35.376296: Update documentation and README
 2025-01-28 08:58:54.447656: Remove deprecated functions
+2025-02-20 10:19:08.454722: Update documentation and README
