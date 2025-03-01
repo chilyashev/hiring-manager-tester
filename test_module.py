3bcd9307fd74723b4f76973934f842094a62bac1
Mihail Chilyashev <chilyashev@gmail.com>
2025-03-01 04:52:34 +0200

    Update documentation and README

--- test_module.py
+++ test_module.py
@@ -64,3 +64,4 @@
 2024-03-26 07:39:09.602173: Remove deprecated functions
 2024-07-18 02:07:21.617488: Improve error handling
 2025-01-15 04:34:18.010448: Add new feature to user interface
+2025-03-01 04:52:34.017348: Update documentation and README
