d6bf2c2cc94410a9001038eb47c0a8c0de9a5358
Mihail Chilyashev <chilyashev@gmail.com>
2025-03-15 22:12:35 +0200

    Refactor code for better readability

--- utils.py
+++ utils.py
@@ -66,3 +66,4 @@
 2025-03-08 23:35:56.197837: Remove deprecated functions
 2025-01-13 20:43:33.326996: Refactor code for better readability
 2025-01-22 16:15:52.348062: Optimize database queries
+2025-03-15 22:12:35.390899: Refactor code for better readability
